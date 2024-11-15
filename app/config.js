a84c8919d1c96c871e38e123981793f8b560abee
James Lattimore <james.lattimore@nhsbsa.nhs.uk>
2024-11-15 12:12:19 +0000

    Demoing error messages on the Start date (future) option...

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'NHS.UK prototype kit',
+  serviceName: 'Check if you have an exemption from paying NHS costs',
 
   // Port to run nodemon on locally
   port: 2000,
