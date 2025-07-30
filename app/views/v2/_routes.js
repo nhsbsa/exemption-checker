c4a06b6131cb40a181bea3ee87cf86c246235330
James Lattimore <james.lattimore@nhsbsa.nhs.uk>
2025-07-30 14:31:53 +0100

    Adding luxon and age checking to the prototype.

--- app/views/v2/_routes.js
+++ app/views/v2/_routes.js
@@ -1,6 +1,7 @@
 // External dependencies
 const express = require('express');
 const router = express.Router();
+const {DateTime} = require('luxon');
 
 
 router.post(/configure-prototype/, function (req, res) {
@@ -9,7 +10,44 @@ router.post(/configure-prototype/, function (req, res) {
 });
 
 router.post(/date-of-birth/, function (req, res) {
-    const destination = 'name';
+    
+    let destination = 'name';
+
+    let day = req.session.data.dateOfBirth.day;
+    let month = req.session.data.dateOfBirth.month;
+    let year = req.session.data.dateOfBirth.year;
+
+
+    if( day && !Number.isNaN(day) && month && !Number.isNaN(month) && year && !Number.isNaN(year) ){
+
+        day = ( day.length === 1 ) ? '0'+day : day;
+        month = ( month.length === 1 ) ? '0'+month : month;
+
+        let date = year + '-' + month + '-' + day;
+
+        const birthDate = DateTime.fromISO( date ); // YYYY-MM-DD
+        const currentDate = DateTime.now();
+
+        let age = currentDate.year - birthDate.year;
+
+        if ( currentDate.month < birthDate.month || (currentDate.month === birthDate.month && currentDate.day < birthDate.day)) {
+            age--;
+        }
+
+        if( age > 59 ){
+            destination = 'confirmation--60-or-over';
+        } else if( age < 16 ){
+            destination = 'confirmation--under-16';
+        }
+
+    } else {
+        if( req.session.data.type === 'confirmation--60-or-over' || req.session.data.type === 'confirmation--under-16' ){
+            destination = req.session.data.type;
+        }
+    }
+
+    
+
     res.redirect( destination );
 });
 
