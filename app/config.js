d00a962ca17e03924e72d013d288f0478e479961
James Lattimore <james.lattimore@nhsbsa.nhs.uk>
2026-02-16 13:46:56 +0000

    Updating to v8 of the prototype kit.

--- app/config.js
+++ app/config.js
@@ -5,15 +5,5 @@ module.exports = {
   serviceName: 'Check if you have an exemption from paying NHS costs',
 
   // Port to run nodemon on locally
-  port: 2000,
-
-  // Automatically stores form data, and send to all views
-  useAutoStoreData: 'true',
-
-  // Enable cookie-based session store (persists on restart)
-  // Please note 4KB cookie limit per domain, cookies too large will silently be ignored
-  useCookieSessionStore: 'false',
-
-  // Enable or disable built-in docs and examples.
-  useDocumentation: true,
+  port: 3000
 };
